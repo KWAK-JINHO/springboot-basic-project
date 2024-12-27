12f0f25fd8c9023f2889a8b7d483f81c0b56f750
KWAK-JINHO <kjhkjhkjh1012@gmail.com>
2024-12-28 02:34:34 +0900

    프로젝트 환경세팅

---
+++ src/test/java/com/jinoprac/springboot_prac/SpringbootPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinoprac.springboot_prac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
