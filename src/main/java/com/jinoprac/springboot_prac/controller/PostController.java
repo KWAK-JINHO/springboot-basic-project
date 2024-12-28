b327604762caa751b30f754cf47c07b8839f86bf
KWAK-JINHO <kjhkjhkjh1012@gmail.com>
2024-12-29 02:42:48 +0900

    LearningSummary.md 추가

---
+++ src/main/java/com/jinoprac/springboot_prac/controller/PostController.java
@@ -0,0 +1,9 @@
+package com.jinoprac.springboot_prac.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PostController {
+
+    public
+}
---
+++ src/main/java/com/jinoprac/springboot_prac/entity/Post.java
@@ -0,0 +1,14 @@
+package com.jinoprac.springboot_prac.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+}
