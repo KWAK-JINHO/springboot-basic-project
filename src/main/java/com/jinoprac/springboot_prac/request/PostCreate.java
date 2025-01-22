ec5fbbef9551dcc6a22aa15d9a364e5f0456ea5e
KWAK-JINHO <kjhkjhkjh1012@gmail.com>
2025-01-22 17:02:02 +0900

    refactor: 게시글 작성 시 제목,내용 글자수 제한

--- src/main/java/com/jinoprac/springboot_prac/request/PostCreate.java
+++ src/main/java/com/jinoprac/springboot_prac/request/PostCreate.java
@@ -2,6 +2,7 @@ package com.jinoprac.springboot_prac.request;
 
 import com.jinoprac.springboot_prac.exception.InvalidRequest;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,9 +14,11 @@ import lombok.Value;
 public class PostCreate {
 
     @NotBlank(message = "제목은 필수 입력값 입니다.")
+    @Size(min = 1, max = 15, message = "제목은 1글자 이상 15글자 이하여야 합니다.")
     String title;
 
     @NotBlank(message = "내용은 필수 입력값 입니다.")
+    @Size(min = 1, max = 1000, message = "내용은 1자이상 1000자 이하여야 합니다.")
     String content;
 
     public void validate() {
