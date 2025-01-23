2997eb06455bda9baac861a2966dffb1a53c0b08
KWAK-JINHO <kjhkjhkjh1012@gmail.com>
2025-01-23 10:46:47 +0900

    refactor: 게시글 수정 시 제목,내용 글자수 제한

--- src/main/java/com/jinoprac/springboot_prac/request/PostEdit.java
+++ src/main/java/com/jinoprac/springboot_prac/request/PostEdit.java
@@ -1,6 +1,8 @@
 package com.jinoprac.springboot_prac.request;
 
+import com.jinoprac.springboot_prac.validator.ForbiddenWords;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Value
@@ -8,9 +10,12 @@ import lombok.*;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class PostEdit {
 
-    @NotBlank(message = "타이틀을 입력하세요.")
+    @NotBlank(message = "제목은 필수 입력값 입니다.")
+    @Size(min = 1, max = 15, message = "제목은 1글자 이상 15글자 이하여야 합니다.")
+    @ForbiddenWords(words = {"무료", "최저가", "할인", "구매"})
     String title;
 
-    @NotBlank(message = "콘텐츠를 입력해주세요.")
+    @NotBlank(message = "내용은 필수 입력값 입니다.")
+    @Size(min = 1, max = 1000, message = "내용은 1자이상 1000자 이하여야 합니다.")
     String content;
 }
