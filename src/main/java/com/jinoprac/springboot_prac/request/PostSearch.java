e00154503a676f4c3af8ef37fc429b18eed0d7ec
KWAK-JINHO <kjhkjhkjh1012@gmail.com>
2025-01-23 11:45:55 +0900

    refactor: 검색 키워드는 공백을 제외하고 최소 1글자 이상이어야 한다.

--- src/main/java/com/jinoprac/springboot_prac/request/PostSearch.java
+++ src/main/java/com/jinoprac/springboot_prac/request/PostSearch.java
@@ -1,6 +1,7 @@
 package com.jinoprac.springboot_prac.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Value
@@ -9,5 +10,6 @@ import lombok.*;
 public class PostSearch {
 
     @NotBlank(message = "키워드를 입력해주세요.")
+    @Size(min = 1, max = 20, message = "최소 1글자 이상 입력해야 합니다.")
     String keyword;
 }
