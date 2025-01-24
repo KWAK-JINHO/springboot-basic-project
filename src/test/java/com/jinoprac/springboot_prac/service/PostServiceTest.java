f51674dc25574333a5fdfdc5906d93da164eed50
KWAK-JINHO <kjhkjhkjh1012@gmail.com>
2025-01-24 16:30:02 +0900

    refactor: 테스트 코드 파일 한개로 통합

--- src/test/java/com/jinoprac/springboot_prac/service/PostServiceTest.java
+++ src/test/java/com/jinoprac/springboot_prac/service/PostServiceTest.java
@@ -5,6 +5,7 @@ import com.jinoprac.springboot_prac.exception.PostNotFound;
 import com.jinoprac.springboot_prac.repository.post.PostRepository;
 import com.jinoprac.springboot_prac.request.PostCreate;
 import com.jinoprac.springboot_prac.request.PostEdit;
+import com.jinoprac.springboot_prac.request.PostSearch;
 import com.jinoprac.springboot_prac.response.PostGetResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -12,7 +13,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -198,5 +198,50 @@ class PostServiceTest {
         assertThrows(PostNotFound.class, () -> {
             postService.deletePost(post.getId() + 1L);
         });
+    }
+
+    @Test
+    @DisplayName("게시글 키워드 검색 테스트")
+    void 게시글_검색기능_성공_테스트() {
+        // given
+        List<Post> posts = List.of(
+                Post.builder().title("제목후후").content("내용후후").build(),
+                Post.builder().title("제목후후").content("내용하하").build(),
+                Post.builder().title("제후목").content("내용후후").build(),
+                Post.builder().title("제목후").content("내용캬캬").build()
+        );
+        postRepository.saveAll(posts);
+        PostSearch postSearch = new PostSearch("후후");
+
+        // when
+        List<PostGetResponse> responses = postService.searchPost(postSearch);
+
+        // then
+        assertFalse(responses.isEmpty());
+        assertEquals(3, responses.size());
+        assertTrue(responses.stream()
+                .allMatch(response -> response.title().contains("후후") || response.content().contains("후후"))
+        );
+
+    }
+
+    @Test
+    @DisplayName("게시글 검색시 키워드가 존재하지 않아요")
+    void 게시글_검색기능_검색키워드가_존재하지_않아요() {
+        // given
+        List<Post> posts = List.of(
+                Post.builder().title("제목후후").content("내용후후").build(),
+                Post.builder().title("제목후후").content("내용하하").build(),
+                Post.builder().title("제후후목").content("내용히히").build(),
+                Post.builder().title("제목후").content("내용캬캬").build()
+        );
+        postRepository.saveAll(posts);
+        PostSearch postSearch = new PostSearch("gngn");
+
+        // when
+        List<PostGetResponse> responses = postService.searchPost(postSearch);
+
+        // then
+        assertTrue(responses.isEmpty());
     }
 }
