7073335cb2f4df9c3d4850bb6937f5f06364751c
KWAK-JINHO <kjhkjhkjh1012@gmail.com>
2025-01-25 23:26:15 +0900

    docs: Rest Docs를 이용한 문서화

---
+++ src/test/java/com/jinoprac/springboot_prac/controller/PostControllerDocTest.java
@@ -0,0 +1,89 @@
+package com.jinoprac.springboot_prac.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jinoprac.springboot_prac.entity.Post;
+import com.jinoprac.springboot_prac.repository.post.PostRepository;
+import com.jinoprac.springboot_prac.request.PostCreate;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.restdocs.payload.PayloadDocumentation;
+import org.springframework.restdocs.request.RequestDocumentation;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs(uriScheme = "https", uriHost = "api.jinoboard.com", uriPort = 443)
+@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
+public class PostControllerDocTest {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Test
+    @DisplayName("글 단건 조회")
+    void 게시글_단건_조회() throws Exception {
+        // given
+        Post post = Post.builder()
+                .title("제목")
+                .content("내용")
+                .build();
+        postRepository.save(post);
+
+        // expected
+        mockMvc.perform(RestDocumentationRequestBuilders.get("/posts/{postId}", 1L)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isOk())
+                .andDo(document("index", RequestDocumentation.pathParameters(
+                                RequestDocumentation.parameterWithName("postId").description("게시글 ID")
+                            ),
+                        PayloadDocumentation.responseFields(
+                                PayloadDocumentation.fieldWithPath("id").description("게시글 ID"),
+                                PayloadDocumentation.fieldWithPath("title").description("제목"),
+                                PayloadDocumentation.fieldWithPath("content").description("내용"),
+                                PayloadDocumentation.fieldWithPath("createAt").description("생성시간")
+                        )
+                        ));
+    }
+
+    @Test
+    @DisplayName("게시글 등록")
+    void 게시글_작성() throws Exception {
+        // given
+        PostCreate postCreate = new PostCreate("제목입니다", "내용입니다");
+
+        // expected
+        mockMvc.perform(RestDocumentationRequestBuilders.post("/posts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(postCreate))
+                )
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isCreated())
+                .andDo(document("index",
+                        PayloadDocumentation.requestFields(
+                                PayloadDocumentation.fieldWithPath("title").description("제목"),
+                                PayloadDocumentation.fieldWithPath("content").description("내용")
+                        )
+                        ));
+    }
+}
